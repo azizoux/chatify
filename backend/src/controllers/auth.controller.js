e6665264fccdf19e49f87b86abe4c196e797da4e
azizoux <azizmahamat@gmail.com>
2026-08-04 09:35:11 +0100

    signup refactoring done

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -42,15 +42,16 @@ export const signup = async (req, res) => {
       password: hashedPassword,
     });
     if (newUser) {
-      generateToken(newUser._id, res);
-      await newUser.save();
+      const savedUser = await newUser.save();
+      generateToken(savedUser._id, res);
+
       res.status(201).json({
         success: true,
         data: {
-          _id: newUser._id,
-          fullName: newUser.fullName,
-          email: newUser.email,
-          profilePic: newUser.profilePic,
+          _id: savedUser._id,
+          fullName: savedUser.fullName,
+          email: savedUser.email,
+          profilePic: savedUser.profilePic,
         },
 
         message: "User created success",
--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -2,7 +2,10 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    const { MONGODB_URI } = process.env;
+    if (!MONGODB_URI) throw Error("MONGODB_URI is not set");
+
+    const conn = await mongoose.connect(MONGODB_URI);
     console.log("MONGO CONNECTED:", conn.connection.host);
   } catch (error) {
     console.error("Error connection to MONGODB:", error);
