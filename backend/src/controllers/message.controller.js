d0a5ed00dc79e62e892329da06fb8b9bd4fe2f86
azizoux <azizmahamat@gmail.com>
2026-08-04 15:12:22 +0100

    message controller added

--- backend/src/controllers/message.controller.js
+++ backend/src/controllers/message.controller.js
@@ -1,3 +1,90 @@
-export const send = (req, res) => {
-  res.send("Send endpoint...");
+import cloudinary from "../lib/cloudinary.js";
+import Message from "../models/Message.js";
+import User from "../models/User.js";
+
+export const getAllContacts = async (req, res) => {
+  try {
+    const loggedInUser = req.user._id;
+    const filteredUsers = await User.find({
+      _id: { $ne: loggedInUser },
+    }).select("-password");
+    res.status(200).json({ success: true, users: filteredUsers });
+  } catch (error) {
+    console.error("Error in getAllContacts controller:", error);
+    res.status(500).json({ success: false, message: "Internal server error" });
+  }
+};
+export const getChatPartners = async (req, res) => {
+  try {
+    const loggedInUserId = req.user._id;
+
+    // find all the messages where the logged-in user is either
+    // sender or receiver
+    const messages = await Message.find({
+      $or: [{ senderId: loggedInUserId }, { receiverId: loggedInUserId }],
+    });
+
+    const chatPartnerIds = [
+      ...new Set(
+        messages.map((msg) =>
+          msg.senderId.toString() === loggedInUserId.toString()
+            ? msg.receiverId.toString()
+            : msg.senderId.toString(),
+        ),
+      ),
+    ];
+
+    const chatPartners = await User.find({
+      _id: { $in: chatPartnerIds },
+    }).select("-password");
+
+    res.status(200).json({ success: true, chatPartners });
+  } catch (error) {
+    console.error("Error in getChatPartners controller:", error);
+    res.status(500).json({ success: false, message: "Server error" });
+  }
+};
+export const getMessagesByUserId = async (req, res) => {
+  try {
+    const myId = req.user._id;
+    const { id: userToChatId } = req.params;
+
+    const messages = await Message.find({
+      $or: [
+        { senderId: myId, receiverId: userToChatId },
+        { senderId: userToChatId, receiverId: myId },
+      ],
+    });
+    res.status(200).json({ success: true, messages });
+  } catch (error) {
+    console.error("Error in getMessagesByUserId controller:", error);
+    res.status(500).json({ success: false, message: "Server error" });
+  }
+};
+export const sendMessage = async (req, res) => {
+  try {
+    const { text, image } = req.body;
+    const { id: receiverId } = req.params;
+    const senderId = req.user._id;
+
+    let imageUrl;
+    if (image) {
+      // upload base64 image to cloudinary
+      const uploadResponse = await cloudinary.uploader.upload(image);
+      imageUrl = uploadResponse.secure_url;
+    }
+    const newMessage = new Message({
+      senderId,
+      receiverId,
+      text,
+      image: imageUrl,
+    });
+    await newMessage.save();
+
+    // todo: send message in real-time if user is online - socket.io
+    res.status(201).json({ success: true, message: newMessage });
+  } catch (error) {
+    console.error("Error in sendMessage controller:", error);
+    res.status(500).json({ success: false, message: "Server error" });
+  }
 };
